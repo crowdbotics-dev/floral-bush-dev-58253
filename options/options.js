d505d0aff8af8adea2a642015a9a04ef889d2481
Crowdbotics <engineering@crowdbotics.com>
2023-01-20 02:04:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-bush-dev-58253",
+  url: "https://floral-bush-dev-58253.botics.co",
+  api: "https://floral-bush-dev-58253.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
